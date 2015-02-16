8135ec66ef8e8583e4ae9ff3b4f6cad9b43c5140
John Weis <weis.john@gmail.com>
2015-02-15 21:25:28 -0500

    progress: cleanup crawl, normalize htmlrefs, refactor code layout

--- crawl.go
+++ crawl.go
@@ -7,10 +7,14 @@ package main
 import (
 	"crypto/sha1"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
+
+	"github.com/weisjohn/htmlrefs"
 )
 
 func bolt(message string) {
@@ -18,10 +22,72 @@ func bolt(message string) {
 	os.Exit(1)
 }
 
+// if the contenttype is not known, normazlie the contenttype based on the server-response
+func divine(contenttype, header string) string {
+
+	// split the mime-type descript on the /, e.g. text/html == "html"
+	header = strings.Split(header, "/")[1]
+	header = strings.Split(header, ";")[0]
+
+	// if the contenttype is not explicitly set, determine from header
+	if contenttype != header {
+		fmt.Printf("content-type: %q != server-sent: %q\n", contenttype, header)
+	}
+
+	if contenttype != "" {
+		return contenttype
+	}
+
+	return header
+}
+
+// read the body, return a base-64 sha1 of the contents
+func hashBody(httpBody io.Reader, url string) string {
+
+	// read the body of the page into a string
+	body, err := ioutil.ReadAll(httpBody)
+	if err != nil {
+		fmt.Printf("Error reading body", url, err)
+		return "foo"
+	}
+
+	// get sha1 of that content
+	hash := sha1.New()
+	hash.Write(body)
+	bodysha := hash.Sum(nil)
+	return fmt.Sprintf("%x", bodysha)
+}
+
+// a simple map of html tokens to content types
+var resourcemap = map[string]string{
+	"link":   "css",
+	"script": "javascript",
+	"a":      "html",
+}
+
+// resources
+type Resource struct{ URI, Type string }
+
+// return a slice of urls to contenttypes
+func getRefs(contenttype string, httpBody io.Reader) []Resource {
+
+	resources := make([]Resource, 0)
+
+	switch contenttype {
+	case "html":
+		refs := htmlrefs.All(httpBody)
+		for _, ref := range refs {
+			resources = append(resources, Resource{URI: ref.URI, Type: resourcemap[ref.Token]})
+		}
+	}
+
+	return resources
+}
+
 // this is a simple map of urls to sha1 hashes
 var visited = make(map[string]string)
 
-func Crawl(url string) {
+func Crawl(url, contenttype string) {
 
 	// if we have already visited it, bounce
 	if _, ok := visited[url]; ok {
@@ -35,24 +101,23 @@ func Crawl(url string) {
 		return
 	}
 
-	// read the body of the page into a string
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("Error reading body", url, err)
-		return
-	}
-	defer resp.Body.Close()
+	// discover conflict between referenced content-type and server reply
+	contenttype = divine(contenttype, resp.Header["Content-Type"][0])
 
-	// get sha1 of that content
-	hash := sha1.New()
-	hash.Write(body)
-	bodysha := hash.Sum(nil)
+	// grab the refs depending on the type of content
+	refs := getRefs(contenttype, resp.Body)
+
+	for _, ref := range refs {
+		fmt.Println(ref)
+	}
 
 	// once we have the sha1, put it into the map
 	// this helps us not refetch, and also we'll persist this later (in the db)
-	visited[url] = fmt.Sprintf("%x", bodysha)
-
+	visited[url] = hashBody(resp.Body, url)
+	defer resp.Body.Close()
 	// TODO: don't follow links that don't match this domain, or something like that...
+
+	// TODO: recursion: fan out, fan in
 
 }
 
@@ -72,6 +137,6 @@ func main() {
 		bolt("You must pass a valid URL")
 	}
 
-	Crawl(first)
+	Crawl(first, "")
 	fmt.Println(visited)
 }
